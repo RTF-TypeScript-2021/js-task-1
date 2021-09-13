e413b7857660efd8d2d22d3d54c5ef6289dcfb4d
Nikita Zernyukov <Nikita.Zerniukov@urfu.me>
2021-09-13 18:52:20 +0500

    Remove hash check-pass

--- topic-1/task-5/specs/task-5.spec.ts
+++ topic-1/task-5/specs/task-5.spec.ts
@@ -1,22 +1,13 @@
 import hash from "hash-it";
 
 const {kaprekarConstant} = require('../index')
-const x = 5408577095486;
+
 
 test('Kaprekar implemented correctly', () => {
-    if(hash(kaprekarConstant) === x){
-        expect(true).toBe(true)
-        return;
-    }
-
     expect(kaprekarConstant(4710)).toBe(6174);
 })
 
 test('On edge-data throws error', () => {
-    if(hash(kaprekarConstant) === x){
-        expect(true).toBe(true)
-        return;
-    }
     expect(() => {kaprekarConstant(6666)}).toThrow();
     expect(() => {kaprekarConstant(12)}).toThrow();
     expect(() => {kaprekarConstant(999999)}).toThrow();
