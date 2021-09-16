4dc9dfa2f13ac332744e279578f0fb6cd2b284e7
DESKTOP-JIA02LP\Raph <abdyev_r@mail.ru>
2021-09-17 00:06:37 +0500

    Выполнил task-1

--- topic-1/task-1/index.js
+++ topic-1/task-1/index.js
@@ -11,6 +11,10 @@
  */
 
  function isTriangle(a, b, c) {
+     if ((a + b) >= c && (b + c) >= a && (a + c) >= b) {
+         return true;
+     }
+     return false;
 }
 
 module.exports.isTriangle = isTriangle;
