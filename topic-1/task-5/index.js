e9f374f614e3dabf8df340ceda654d65f27cc066
makar <misha.torno@gmail.com>
2021-09-16 14:18:07 +0500

    решил пятый таск

--- topic-1/task-5/index.js
+++ topic-1/task-5/index.js
@@ -4,7 +4,29 @@
  * Предусмотреть крайние случаи, для входных данных и при необходимости
  * выкидывать исключение.
  * */
-function kaprekarConstant(number){
+
+function kaprekarConstant(number) {
+    if (number < 1000)
+        throw new SyntaxError("число должно быть четерыхзначным");
+
+    while (number !== 6174) {
+        let array = [];
+
+        while (number > 0) {
+            array.push(number % 10);
+            number = Math.floor(number / 10);
+        }
+
+        let big = parseInt(array.sort((a, b) => b - a).join(""));
+        let small = parseInt(array.sort().join(""));
+
+        number = big - small;
+
+        if (number === 0)
+            throw new SyntaxError("число не может состоять из одинаковых символов");
+    }
+
+    return number;
 }
 
 module.exports.kaprekarConstant = kaprekarConstant;
