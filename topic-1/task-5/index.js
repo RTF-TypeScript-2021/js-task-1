20a99bb2e328d7130263d15564281b627674e4bb
Vladislav <83534627+BJlADOS@users.noreply.github.com>
2021-09-18 18:46:19 +0500

    Фикс 5 задачи

--- topic-1/task-5/index.js
+++ topic-1/task-5/index.js
@@ -4,39 +4,40 @@
  * Предусмотреть крайние случаи, для входных данных и при необходимости
  * выкидывать исключение.
  * */
-function kaprekarConstant(number) 
-{    
+ function kaprekarConstant(number) 
+ {    
     if (number > 9999 || number < 1000) throw new Error("Число не четырёхзначное"); 
-    for (let i = 1, b = number.toString(), counter = 0; i < b.length; i++) {
-        if (b[0] == b[i])
+    number = number.toString();
+    for (let i = 1, counter = 0; i < number.length; i++) {
+        if (number[0] == number[i])
             counter++;
         if (counter == 3) throw new Error("Все цифры в числе одинаковы");
     } 
-    while(true) 
+    while(true)
     {
-        number = calculate(number.toString());
-
-        if (number == 6174) return number;
+        number = calculate(number);             
+        if (number == "6174") return parseInt(number);
     }
 }
 
-function calculate(number) 
-{
+function calculate(number) {
     let sub = number.split('').sort((a, b) => a - b);
     let num1 = sub.join('');
     let num2 = sub.reverse().join('');
     num1 = parseInt(num1);
-    num2 = parseInt(num2) - num1; 
+    num2 = parseInt(num2) - num1;
+    if (num2 < 1000)
+    { 
+        return fixNumber(num2.toString().split(''));
+    }
     return num2.toString();
 }
 
-function fixNumber(number) 
+function fixNumber(number)
 {
-    let sub = number.toString();
     for (let i = 0; i < 4 - number.length; i++) 
-    {
-        sub.unshift("0");
-    }
-
+        number.unshift("0");
+    return number.join('');
 }
+
 module.exports.kaprekarConstant = kaprekarConstant;
