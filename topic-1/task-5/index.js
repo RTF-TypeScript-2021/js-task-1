0feba11ccc886e3499988d6a832e4756fd0e24f4
Vladislav <83534627+BJlADOS@users.noreply.github.com>
2021-09-18 18:36:22 +0500

    поправил пятую задачку

--- topic-1/task-5/index.js
+++ topic-1/task-5/index.js
@@ -4,25 +4,40 @@
  * Предусмотреть крайние случаи, для входных данных и при необходимости
  * выкидывать исключение.
  * */
-function kaprekarConstant(number) {    
+console.log(kaprekarConstant(1112));
+function kaprekarConstant(number) 
+{    
     if (number > 9999 || number < 1000) throw new Error("Число не четырёхзначное"); 
     for (let i = 1, b = number.toString(), counter = 0; i < b.length; i++) {
         if (b[0] == b[i])
             counter++;
         if (counter == 3) throw new Error("Все цифры в числе одинаковы");
     } 
-    while(true)
+    while(true) 
     {
-        number = calculate(number);             
+        number = calculate(number.toString());
+
         if (number == 6174) return number;
     }
 }
-function calculate(number) {
-    let sub = number.toString().split('').sort((a, b) => a - b);
+
+function calculate(number) 
+{
+    let sub = number.split('').sort((a, b) => a - b);
     let num1 = sub.join('');
     let num2 = sub.reverse().join('');
     num1 = parseInt(num1);
     num2 = parseInt(num2) - num1; 
-    return num2;
+    return num2.toString();
+}
+
+function fixNumber(number) 
+{
+    let sub = number.toString();
+    for (let i = 0; i < 4 - number.length; i++) 
+    {
+        sub.unshift("0");
+    }
+
 }
 module.exports.kaprekarConstant = kaprekarConstant;
