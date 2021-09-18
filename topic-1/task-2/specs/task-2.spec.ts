5c203f134d84980ca4217d146f3272de5a0b06a9
Vsevolod Pyatkov <PyatkovVsevolod@yandex.ru>
2021-09-18 23:13:43 +0500

    Переделал задания

--- topic-1/task-2/specs/task-2.spec.ts
+++ topic-1/task-2/specs/task-2.spec.ts
@@ -4,4 +4,5 @@ test('Method works fine', () => {
     expect(calculateDoubleArray([1, 2, 3, 4, 5, 0, 0, 0, 0, 0])).toEqual([1, 1, 2, 2, 3, 3, 4, 4, 5, 5]);
     expect(calculateDoubleArray([0, 0, 0, 0, 0])).toEqual([]);
     expect(calculateDoubleArray([1, 0])).toEqual([1, 1]);
+    expect(calculateDoubleArray([1, 1, 5, 10, 0, 1, 2])).toEqual([1, 1, 1, 1, 5, 5, 10, 10, 1, 1, 2, 2]);
 });
