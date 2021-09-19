7b3c8f6bb824bff86f33fa9246719104a31af356
alexeyvolhin <82944068+alexeyvolhin@users.noreply.github.com>
2021-09-19 23:39:12 +0500

    Third

--- topic-1/task-3/index.js
+++ topic-1/task-3/index.js
@@ -10,6 +10,14 @@
 */
 
 function factorial(n) {
+    if (n < 0){
+        throw new Error("Value must be more than zero"); 
+    }
+    else if (n === 0) {
+        return 1;
+    }
+    else {
+    return n * factorial(n - 1);
+    }
 }
-
 module.exports.factorial = factorial;
