f15638d0a55865fce869c07539caf5f564d2357e
Kaigarodov Andrey <kaigarodov2012@yandex.ru>
2021-09-19 22:09:09 +0500

    fix: check input for 3332/3343 numbers

--- topic-1/task-5/index.js
+++ topic-1/task-5/index.js
@@ -5,16 +5,29 @@
  * выкидывать исключение.
  * */
 function kaprekarConstant(number) {
-    if(!Number.isInteger(number)) throw new Error(`the input must be Integer, but get ${typeof number}`);
-    else if(number < 1001 || number > 9999) throw new Error("value is not in 999-10000");
-    else if(new Set(number.toString(10).split("")).size === 1){
-        throw new Error("bad number XXXX"); 
-    } 
-    for (; ;) {
+    /*Очень не хотел дублировать код, но единственный простой
+        способ проверить number на числа XXXX или XXYX где Y = X+-1
+        Это найти разность между возможным максимумом и минимумом из этих чисел, 
+        что и является основным действием алгоритма*/ 
+    function getDiff(number){
         max = number.toString().split("").sort((lv, rv) => (rv - lv)); 
         min =  max.slice(0).reverse();
-        diff = Number(max.join("")) - Number(min.join(""));
-        if (diff == number) break;
+        return Number(max.join("")) - Number(min.join(""));
+    }
+    if(!Number.isInteger(number)){
+        throw new Error(`the input must be Integer, but get ${typeof number}`);
+    } 
+    else if(number < 1001 || number > 9999){
+        throw new Error("value is not in 999-10000");
+    }
+    else if(getDiff(number) === 0 || getDiff(number) === 999){
+        throw new Error("bad number XXXX or XYXX where Y= X+-1"); 
+    } 
+    for (; ;) {
+        diff = getDiff(number); 
+        if (diff == number){
+            break;
+        }
         number = diff;
     }
     return diff;
