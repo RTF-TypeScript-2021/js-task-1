3cad94c1f1782464ececb44539baeb20596296ea
Kaigarodov Andrey <kaigarodov2012@yandex.ru>
2021-09-20 01:26:44 +0500

    fix: 4443 not equal 0

--- topic-1/task-5/index.js
+++ topic-1/task-5/index.js
@@ -10,18 +10,22 @@ function kaprekarConstant(number) {
         Это найти разность между возможным максимумом и минимумом из этих чисел, 
         что и является основным действием алгоритма*/ 
     function getDifference(number) {
-        max = number.toString().split("").sort((lv, rv) => (rv - lv)); 
+        numberArr = number.toString().split(""); 
+        if (number<1000) {
+            numberArr.push("0");
+        } 
+        max = numberArr.slice(0).sort((lv, rv) => (rv - lv)); 
         min =  max.slice(0).reverse();
         return Number(max.join("")) - Number(min.join(""));
     }
     if (!Number.isInteger(number)) {
         throw new Error(`the input must be Integer, but get ${typeof number}`);
     } 
-    else if (number < 1001 || number > 9999) {
+    else if (number < 1000 || number > 9999) {
         throw new Error("value is not in 999-10000");
     }
-    else if (getDifference(number) === 0 || getDifference(number) === 999) {
-        throw new Error("bad number XXXX or XYXX where Y= X+-1"); 
+    else if (getDifference(number) === 0) {
+        throw new Error("bad number XXXX"); 
     } 
     for (;;) {
         diff = getDifference(number); 
