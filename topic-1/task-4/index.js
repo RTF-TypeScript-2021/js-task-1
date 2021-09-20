4d141094ece3264cf927bab5fbca4dc9270064b1
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2021-09-21 00:33:17 +0500

    Савельев Клим
    js-task-4 fix

--- topic-1/task-4/index.js
+++ topic-1/task-4/index.js
@@ -5,7 +5,19 @@
  * @return {number} Количество уникальных имён
  */
 
-const countUniqueName = nameArray =>
-    new Set(nameArray.filter(name => name.match("^[A-Z;a-z;А-Я;а-я;.]+$"))).size;
+const countUniqueName = nameArray => { 
+    return new Set(
+        nameArray
+        // Плохое и костыльное решение с разбиением слова с \n на два с помощью flatMap, 
+        // скорее всего это можно решить лучше с regexp, но голова уже не мыслит и дедлайн прошёл
+            .flatMap(x => x.includes("\n") 
+                ? [x.substr(0, x.indexOf("\n")), 
+                    x.substr(x.indexOf("\n"), x.length - 1).replace(/\n/g,"")]
+                : x
+            )
+            .map(name => name.toLowerCase())
+            .filter(name => name.match("^[A-Z;a-z;А-Я;а-я;.;Ё-ё]*$"))
+    ).size;
+}
 
 module.exports.countUniqueName = countUniqueName;
