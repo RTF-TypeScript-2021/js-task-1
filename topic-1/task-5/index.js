8f5f0a3d812666cfc645df7d5b75805636ee3bbd
nkolomiychyk <n.kolomiychyk@gmail.com>
2021-09-20 20:27:45 +0500

    Посмотреть для Никиты, решенное 5-ое не до конца

--- topic-1/task-5/index.js
+++ topic-1/task-5/index.js
@@ -5,6 +5,27 @@
  * выкидывать исключение.
  * */
 function kaprekarConstant(number){
+    if(number<1000 || number>9999)
+        throw new Error();
+    const diffDigits = new Set(String(number).split(''));
+    if(diffDigits<2)
+        throw new Error();
+    let result = number;
+    while(result!=6174)
+        result = calculateNumber(result);             
+    return result;
+    
+}
+function calculateNumber(result){
+    const strArray = result.toString().split('');
+    const arrNumber = new Array(strArray.length);
+    for (let i = 0; i < strArray.length; i++) {
+        arrNumber[i]= parseInt(strArray[i],10);
+    }
+    const bigNumber = parseInt(arrNumber.sort((a,b)=>b-a).join(''),10);
+    const smallNumber = parseInt(arrNumber.sort((a,b)=>a-b).join(''),10);
+    const resultCalculate = bigNumber - smallNumber;
+    return resultCalculate;
 }
 
 module.exports.kaprekarConstant = kaprekarConstant;
