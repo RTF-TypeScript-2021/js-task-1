b9fa888af1a32b28166a5a5d4c47c6317dbc01e5
6loodrain <89647718+6loodrain@users.noreply.github.com>
2021-09-20 11:17:16 -0700

    Пятая задача

--- topic-1/task-5/index.js
+++ topic-1/task-5/index.js
@@ -5,6 +5,38 @@
  * выкидывать исключение.
  * */
 function kaprekarConstant(number){
+    let splSrtNumber = number.toString().split("").sort();
+    let isCorrect = false;
+
+    for (let i = 1; i < splSrtNumber.length; i++){
+        if(splSrtNumber[i] != splSrtNumber[i - 1]) isCorrect = true;
+    }
+
+    if (!isCorrect || splSrtNumber.length != 4) throw err;
+
+    while (true){
+        let descendingNumber = getDescendingNumber(splSrtNumber);
+        let increasingNumber = getIncreasingNumber(splSrtNumber);
+        if (descendingNumber - increasingNumber == number) return number;
+        number = descendingNumber - increasingNumber;
+        splSrtNumber = number.toString().split("").sort();
+    }
+}
+
+function getDescendingNumber(number){
+    let newNumber = 0;
+    for (let i = number.length - 1; i >= 0; i--){
+        newNumber += number[i];
+    }
+    return Number(newNumber);
+}
+
+function getIncreasingNumber(number){
+    let newNumber = 0;
+    for (let i = 0; i < number.length; i++){
+        newNumber += number[i];
+    }
+    return Number(newNumber);
 }
 
 module.exports.kaprekarConstant = kaprekarConstant;
