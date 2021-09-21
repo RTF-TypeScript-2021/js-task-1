7db1a20ebc36294598a6a75750cedd7f30329327
DESKTOP-7NFU6LQ\Пользователь <utevasvetlana31@gmail.com>
2021-09-21 14:45:49 +0500

    Задача 1 исправлено

--- topic-1/task-1/index.js
+++ topic-1/task-1/index.js
@@ -11,7 +11,9 @@
  */
 
  function isTriangle(a, b, c) {
-     return (a + b > c && a + c > b && b + c > a);
+    if (Number.isInteger(a, b, c) && a > 0 && b > 0 && c > 0) {
+        return (a + b > c && a + c > b && b + c > a);
+    }
 }
 
 module.exports.isTriangle = isTriangle;
