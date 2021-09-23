f4d43f6cf5f2dc1459766f79cce4ca51808530f7
Tanya Koveshnikova <T.A.Koveshnikova@urfu.me>
2021-09-23 16:27:04 +0500

    переделано 1 задание

--- topic-1/task-1/index.js
+++ topic-1/task-1/index.js
@@ -11,7 +11,7 @@
  */
 
  function isTriangle(a, b, c) {
-     if(a > 0 && b > 0 && c > 0)
+     if(a > 0 && b > 0 && c > 0 && Number.isInteger(a, b, c))
      {
          return a + b > c && a + c > b && c + b > a;
      }
