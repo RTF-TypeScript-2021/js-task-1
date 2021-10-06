df4c9dc9b044a22e1309c790e9b515a6f4208e3e
AKozelskikh <a.kozelskikh@artsofte.ru>
2021-10-06 09:53:56 +0500

    ДЗ первой недели

--- topic-1/task-4/index.js
+++ topic-1/task-4/index.js
@@ -6,7 +6,22 @@
  * @param {string[]} nameArray - массив имён.
  * @return {number} Количество уникальных имён
  * */
+
 function countUniqueName(nameArray) {
+
+    var lowerArray = [];
+    for (i = 0; i < nameArray.length - 1; i++) {
+        let name = nameArray[i].toString().toLowerCase().replace(/[; .,!?:–]/, "")
+        if (nameArray[i] != "") {
+            lowerArray.push(name)
+        }
+    }
+
+    const unique = new Set(lowerArray);
+
+    return unique.size
 }
+
+countUniqueName(arrayOfName)
 
 module.exports.countUniqueName = countUniqueName;
--- topic-1/task-5/index.js
+++ topic-1/task-5/index.js
@@ -4,7 +4,35 @@
  * Предусмотреть крайние случаи, для входных данных и при необходимости
  * выкидывать исключение.
  * */
-function kaprekarConstant(number){
+
+function kaprekarConstant(number) {
+    let countDigits = number.toString().length;
+    //Шаг первый: проверка на коиличество цифр. Должно быть 4.
+    if (countDigits === 4) {
+        var digitToArray = number.toString().split("");
+        var arrayToCompareDigit = new Set(digitToArray)
+        //Шаг второй: проверка на коиличество повторений цифр. Должна хотя бы одна не повториться.
+        if (arrayToCompareDigit.size > 1) {
+            var result = number
+            //Проверяем, выполнено ли условие
+            while (result != 6174) {
+                result = result.toString().split("");
+                //Шаг третий: создания чисел из исходного с отсортированными цифрами от большего к меньшему и от меньшего к большему
+                let numberFromBigToSmall = result.sort((a, b) => b - a).join('');
+                let numberFromSmallToBIG = result.sort((a, b) => a - b).join('');
+                //Шаг четвертый: вычетаем из большего меньшее
+                var result = numberFromBigToSmall - numberFromSmallToBIG
+            }
+            return result
+        } else {
+            throw new Error('Число не должно состоять из одинаковых цифр.')
+        }
+    } else {
+        throw new Error('Число не из 4-х цифр.')
+    }
 }
+
+//для отладки
+kaprekarConstant(1234)
 
 module.exports.kaprekarConstant = kaprekarConstant;
